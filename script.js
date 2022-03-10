2ac4d23222ad7a88a2e6afbd4e9ad06094e975e5
Eldio de Almeida Barbosa Neto <97411284+Eldio-Neto@users.noreply.github.com>
2022-03-09 21:14:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+var url = 'https://economia.awesomeapi.com.br/last/USD-BRL'
+
+
+function Converter(){
+    let input = document.getElementById('valor');
+    let valor = input.value;
+    let Resultado = document.getElementById('resultado')
+
+    fetch(url).then((res)=>{
+        return res.json()
+    }).then((data)=>{
+
+        let valorDolar = data.USDBRL.high;
+        let BRLUSD = Math.round((valor / valorDolar)*100) / 100
+
+        if(valor == "" || valor == 0){
+
+            alert('por favor digite um numero maior que 0')
+
+        }
+        else if(valor == 1){
+            Resultado.innerHTML = `Com ${valor} real você consegue comprar ${BRLUSD} dolares`
+            
+        }
+        
+        else if(valor > 1){
+            Resultado.innerHTML = `Com ${valor} reais você consegue comprar ${BRLUSD} dolares`
+            
+        }
+    })     
+
+    document.getElementById('valor').value = ""
+    
+}
+
+
+
+
+
